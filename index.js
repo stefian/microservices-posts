40d107444d08447b2fd7d548d98cb674d04642b7
stefian <stefan.ianta@w3ai.net>
2020-06-04 14:12:15 -0300

    added axios posting PostCreated on Event Bus / 4005

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,7 +16,7 @@ app.get('/posts', (req, res) => {
 });
 
 // create post handler
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;     // get title from the request body
 
@@ -23,9 +24,16 @@ app.post('/posts', (req, res) => {
         id, title
     };
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
+
     res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
-    console.log('Listening on 4000');
+    console.log('Posts: Listening on 4000');
 });
